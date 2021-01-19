cd6569d3b2c5ab4cc1cbdc97e67e59c000823db7
FranckEinstein90 <franckbinard@gmail.com>
2021-01-19 00:29:45 +0000

    added floor

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -13,10 +13,33 @@ export default class Scene extends React.Component {
         super(props);  
     }
     componentDidMount(){
-        this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-	    this.camera.position.z = 1;
+
+        this.scene = new THREE.Scene();
+        this.scene.background = new THREE.Color( 0x222222 );
+
+        this.camera = new THREE.PerspectiveCamera( 50, window.innerWidth / window.innerHeight, 0.01, 50 );
+        this.camera.position.set( 0, 1.6, 3 );
 
 	    this.scene = new THREE.Scene();
+        const floorGometry = new THREE.PlaneBufferGeometry( 4, 4 );
+        const floorMaterial = new THREE.MeshStandardMaterial( {
+            color: 0x222222,
+            roughness: 1.0,
+            metalness: 0.0
+        } );
+        const floor = new THREE.Mesh( floorGometry, floorMaterial );
+        floor.rotation.x = - Math.PI / 2;
+        this.scene.add( floor );
+        
+        const grid = new THREE.GridHelper( 10, 20, 0x111111, 0x111111 );
+        // grid.material.depthTest = false; // avoid z-fighting
+        this.scene.add( grid );
+
+        this.scene.add( new THREE.HemisphereLight( 0x888877, 0x777788 ) );
+
+        const light = new THREE.DirectionalLight( 0xffffff, 0.5 );
+        light.position.set( 0, 4, 0 );
+        this.scene.add( light );
 
 	    const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
 	    const material = new THREE.MeshNormalMaterial();
