052e547c4515b271281a23c31e24d2ef409aeae0
FranckEinstein90 <franckbinard@gmail.com>
2021-01-19 01:30:59 +0000

    refactor

--- src/components/Scene.js
+++ src/components/Scene.js
@@ -34,6 +34,45 @@ export default class Scene extends React.Component {
          this.scene.add( floor );
     }
 
+    addPainters(){
+        this.painter1 = new TubePainter();
+        this.scene.add( this.painter1.mesh );
+
+		this.painter2 = new TubePainter();
+		this.scene.add( this.painter2.mesh );
+    }
+    setControllers(){
+        this.controller1 = this.renderer.xr.getController( 0 );
+/*        this.controller1.addEventListener( 'selectstart', onSelectStart );
+        this.controller1.addEventListener( 'selectend', onSelectEnd );
+        this.controller1.addEventListener( 'squeezestart', onSqueezeStart );
+        this.controller1.addEventListener( 'squeezeend', onSqueezeEnd );*/
+        this.controller1.userData.painter = this.painter1;
+        this.scene.add( this.controller1 );
+
+        this.controller2 = this.renderer.xr.getController( 1 );
+/*        this.controller2.addEventListener( 'selectstart', onSelectStart );
+        this.controller2.addEventListener( 'selectend', onSelectEnd );
+        this.controller2.addEventListener( 'squeezestart', onSqueezeStart );
+        this.controller2.addEventListener( 'squeezeend', onSqueezeEnd );*/
+        this.controller2.userData.painter = this.painter2;
+        this.scene.add( this.controller2 );
+        
+        const geometry = new THREE.CylinderBufferGeometry( 0.01, 0.02, 0.08, 5 );
+        geometry.rotateX( - Math.PI / 2 );
+        const material = new THREE.MeshStandardMaterial( { flatShading: true } );
+        const mesh = new THREE.Mesh( geometry, material );
+
+        const pivot = new THREE.Mesh( new THREE.IcosahedronBufferGeometry( 0.01, 3 ) );
+        pivot.name = 'pivot';
+        pivot.position.z = - 0.05;
+        mesh.add( pivot );
+
+        this.controller1.add( mesh.clone() );
+        this.controller2.add( mesh.clone() );
+
+
+    }
     componentDidMount(){
 
         this.scene = new THREE.Scene();
@@ -59,11 +98,13 @@ export default class Scene extends React.Component {
 	    const mesh = new THREE.Mesh( geometry, material );
 	    this.scene.add( mesh );
 
+        this.addPainters(); 
         this.renderer = new THREE.WebGLRenderer( { antialias: true } );
         this.renderer.setPixelRatio( window.devicePixelRatio );
         this.renderer.setSize( window.innerWidth, window.innerHeight );
         this.renderer.outputEncoding = THREE.sRGBEncoding;
         this.renderer.xr.enabled = true;
+        this.setControllers(); 
         document.body.appendChild( this.renderer.domElement ); 
         document.body.appendChild( VRButton.createButton( this.renderer ) );
         this.animate(); 
