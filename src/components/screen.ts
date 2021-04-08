199a324b5683c53ff0fed7f375674858c9f3113b
FranckEinstein90 <franckbinard@gmail.com>
2021-04-08 01:47:58 +0100

    moved back wall screen up

--- src/components/screen.ts
+++ src/components/screen.ts
@@ -1,10 +1,12 @@
 import * as THREE from 'three'; 
 
 export const screen = (scene : THREE.Scene ) => {
-    const geometry = new THREE.BoxGeometry( 3.2, 3.2, 0.1 );
+    const height=1.8; 
+    const geometry = new THREE.BoxGeometry( 3.2, height,  0.1 );
     const material = new THREE.MeshNormalMaterial();
     const mesh = new THREE.Mesh( geometry, material );
-    mesh.translateZ(-2) ; 
+    mesh.translateZ(-2) ;
+    mesh.translateY(height/2) ; 
     scene.add( mesh );
 } ; 
 
