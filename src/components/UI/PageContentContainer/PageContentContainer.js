5294ae46192f9c9a555b5b019f942d04e4772e62
FranckEinstein90 <franckbinard@gmail.com>
2022-09-07 23:15:14 +0100

    feature: Layout in index.js

---
+++ src/components/UI/PageContentContainer/PageContentContainer.js
@@ -0,0 +1,15 @@
+import * as React from "react"; 
+import Scene from "../../Scene"; 
+
+export default class PageContentContainer extends React.Component {
+
+    constructor( props ){
+      super(props); 
+    }
+  
+    render(){
+  
+      return (
+        <Scene />
+    )}
+  } ; 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,22 @@
 import * as React from "react"
-import Scene from "../components/Scene" ; 
+import PageContentContainer from "../components/UI/PageContentContainer/PageContentContainer" ;
 import styles from "./index.module.css" ; 
 
-const IndexPage = () => {
+
+const Layout = ( props )=> {
+
+  const appName   = 'BuildB4Build'; 
+  const [dialog   , setDialog]    = React.useState(false) ; 
+  const [language , setLanguage ] = React.useState('EN') ; 
+
+  
+
   return (
-    <div className={styles.page}>
-     <Scene/>
+    <div className={styles.pageContainer}> 
+        <PageContentContainer />
     </div>
   )
-}
+} ; 
 
-export default IndexPage
+
+export default Layout ;  
